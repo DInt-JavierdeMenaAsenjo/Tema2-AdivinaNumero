dbd57fa6e4d60704bc17f6164f9886c95a8bbc6f
JAVIER DE MENA ASENJO <javdemase@alu.edu.gva.es>
2022-09-29 11:18:01 +0200

    Agregar archivos de proyecto.

---
+++ Tema2-AdivinaNumero/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_AdivinaNumero
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int numeroAleatorio;
+        public MainWindow()
+        {
+            InitializeComponent();
+            numeroAleatorio = generaNumero();
+            prueba.Text = numeroAleatorio.ToString();
+        }
+
+        public int generaNumero()
+        {
+            Random rnd = new Random();
+            return rnd.Next(101);
+        }
+
+        private void botonComprobar_Click(object sender, RoutedEventArgs e)
+        {
+            if(int.Parse(numeroTextBox.Text) < numeroAleatorio)
+            {
+                pistaTextBlock.Text = "Te has quedado corto";
+            }else if(int.Parse(numeroTextBox.Text) > numeroAleatorio)
+            {
+                pistaTextBlock.Text = "Te has pasado";
+            }else
+            {
+                pistaTextBlock.Text = "ACERTASTE!";
+            }
+        }
+
+        private void botonReiniciar_Click(object sender, RoutedEventArgs e)
+        {
+            numeroAleatorio = generaNumero();
+            pistaTextBlock.Text = "";
+            prueba.Text = numeroAleatorio.ToString();
+        }
+    }
+}
